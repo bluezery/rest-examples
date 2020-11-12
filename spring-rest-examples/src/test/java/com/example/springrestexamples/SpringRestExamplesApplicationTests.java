96a5d75a78db2f2d921803c64cf1855aa00438e4
Taehwan Kim <k.taehwan@linecorp.com>
2020-11-12 18:29:04 +0900

    Init by spring initializer
    References
    - https://spring.io/guides/gs/rest-service/
    - https://start.spring.io/

---
+++ spring-rest-examples/src/test/java/com/example/springrestexamples/SpringRestExamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrestexamples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestExamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
